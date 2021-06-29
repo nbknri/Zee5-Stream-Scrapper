855ce271fa06b4ed9699b99e4a8af3f9f0a9ff83
Avishkar Patil <avishkarpatil29@gmail.com>
2021-06-29 19:31:48 +0530

    Initial Commit

--- index.php
+++ index.php
@@ -1,19 +1,21 @@
 <?php
 
 // Don't Edit , any problems 
-// @Avishkatpatil [ TG ]
+// ©  @Avishkatpatil [ TG ]
 // Star This Repo
 
 $url =$_GET['c'];
 if($url !=""){
 $id = end(explode('/', $url));
 $tlink ="https://gwapi.zee5.com/content/details/$id?translation=en&country=IN&version=2";
-$token =file_get_contents("https://useraction.zee5.com/token/platform_tokens.php?platform_name=web_app");
-$tokn =json_decode($token);
-$tok =$tokn->token;
-$vtoken =file_get_contents("http://useraction.zee5.com/tokennd/");
-$vtokn =json_decode($vtoken);
-$vtok =$vtokn->video_token;
+$tokenurl =file_get_contents("https://useraction.zee5.com/token/platform_tokens.php?platform_name=web_app");
+$tok =json_decode($tokenurl, true);
+$token =$tok['token'];
+
+$vtok =file_get_contents("http://useraction.zee5.com/tokennd/");
+$vtokn =json_decode($vtok, true);
+$vtoken =$vtokn['video_token'];
+
 
 $curl = curl_init();
 curl_setopt_array($curl, array(
@@ -23,28 +25,29 @@ curl_setopt_array($curl, array(
   CURLOPT_FOLLOWLOCATION => true,
   CURLOPT_CUSTOMREQUEST => "GET",
   CURLOPT_HTTPHEADER => array(
-    "x-access-token: $tok",
+    "x-access-token: $token",
     "Content-Type: application/json"
   ),
 ));
 $response = curl_exec($curl);
 curl_close($curl);
 
-$hls =json_decode($response);
-$image =$hls->image_url;
-$title =$hls->title;
-$des =$hls->description;
-$vhls =$hls->hls[0];
-$sub =$hls->vtt_thumbnail_url[0];
-$error =$hls->error_code;
+$z5 =json_decode($response, true);
+$image =$z5['image_url'];
+$title =$z5['title'];
+$des =$z5['description'];
+$vhls =$z5['hls'][0];
+$sub =$z5['vtt_thumbnail_url'][0];
+$error =$z5['error_code'];
 $vidt = str_replace('drm', 'hls', $vhls);
 $img = str_replace('270x152', '1170x658', $image); 
 
- $vid = "https://zee5vodnd.akamaized.net".$vidt.$vtok;
+$hls = "https://zee5vodnd.akamaized.net".$vidt.$vtoken;
+
 header("Content-Type: application/json");
 $errr= array("error" => "Put Here Only ZEE5 Proper URL ,  Invalid Input " );
 $err =json_encode($errr);
-$apii = array("title" => $title, "description" => $des, "thumbnail" => $img, "video_url" => $vid, "subtitle_url" => $sub, "created_by" => "Avishkar Patil");
+$apii = array("title" => $title, "description" => $des, "thumbnail" => $img, "video_url" => $hls, "subtitle_url" => $sub, "created_by" => "Avishkar Patil");
 $api =json_encode($apii, JSON_UNESCAPED_SLASHES);
 if($error ==101){
 echo $err;
@@ -54,6 +57,8 @@ echo $api;
 }
 }
 else{
+  header("Content-Type: application/json");
   echo "Hello There Is Problem In Your Link Or Check Your Link Format !!";
+  // © Avishkar Patil 
 }
-
+?>
